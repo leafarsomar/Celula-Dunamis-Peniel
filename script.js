a56286b5cd824b8991d949ababa90b0e28cd1af4
leafarsomar <rafaelramuzz@gmail.com>
2024-04-04 17:34:06 -0300

    celula

---
+++ script.js
@@ -0,0 +1,21 @@
+const form= document.querySelector(".formulario-fale-conosco")
+const img= document.querySelector(".imagem-lideres")
+
+function mostrarForm(){
+    form.style.left = "440px"
+    form.style.transform = "translatex (-50%)"
+}
+
+function mostrarImg(){
+    img.style.left = "440px"
+    img.style.transform = "translatex (-50%)"
+}
+
+    function esconderForm(){
+        form.style.left = "-325px"
+        
+
+    }
+    function esconderImg(){
+        img.style.left = " 1340px"
+    }
